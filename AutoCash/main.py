c55cb83f603e55ced2b0582ad280958a474370a9
HWS_NEW\Administrator <gksdnjstjr@gmail.com>
2022-08-03 09:37:53 +0900

    main - 수정

---
+++ AutoCash/main.py
@@ -0,0 +1,14 @@
+import global_variable as g
+import macro_gui as gui
+
+def main():
+    
+    gui.show();
+    
+    print(g.kakao_opentalk_name)
+    
+    # while(True):
+    #     print('작업할꺼야')
+        
+if __name__ == '__main__':
+    main();
